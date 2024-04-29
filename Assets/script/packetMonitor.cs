a8c61b445b393228863c24d752eab5911dbd7060
gbox3d <gbox3d@gmail.com>
2024-04-29 19:37:12 +0900

    port select

--- Assets/script/packetMonitor.cs
+++ Assets/script/packetMonitor.cs
@@ -32,11 +32,13 @@ public class packetMonitor : MonoBehaviour
     [SerializeField] private TextMeshProUGUI textDevId;
     [SerializeField] private TextMeshProUGUI textYawPitchRoll;
 
+    [SerializeField] int localPort = 9250;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        udpReceiver = new UDPReceiver(9250); // Use the same port as in your Arduino code
+        udpReceiver = new UDPReceiver(localPort); // Use the same port as in your Arduino code
         cts = new CancellationTokenSource();
         ReceivePacketsAsync(cts.Token);
     }
@@ -53,21 +55,39 @@ public class packetMonitor : MonoBehaviour
 
                 // Receive a packet
                 S_Udp_IMU_RawData_Packet packet = await udpReceiver.ReceivePacketAsync();
-                
-                textNetWorkFps.text = (1.0f / deltaTime).ToString("F2");
 
-                //소숫점 3자리까지만 표시
-                textQuaternionValues.text = string.Format("qW: {0:F3}, qX: {1:F3}, qY: {2:F3}, qZ: {3:F3}", packet.qW, packet.qX, packet.qY, packet.qZ);
-                textAccelValues.text = string.Format("AccelX: {0:F3}, AccelY: {1:F3}, AccelZ: {2:F3}", packet.aX, packet.aY, packet.aZ);
-                textGyroValues.text = string.Format("GyroX: {0:F3}, GyroY: {1:F3}, GyroZ: {2:F3}", packet.gX, packet.gY, packet.gZ);
-                textMagValues.text = string.Format("MagX: {0:F3}, MagY: {1:F3}, MagZ: {2:F3}", packet.mX, packet.mY, packet.mZ);
-                textBattery.text = string.Format("{0:F3}", packet.battery);
-                textFireCount.text = string.Format("{0}", packet.fire_count);
-                textDevId.text = string.Format("{0}", packet.dev_id);
-                textYawPitchRoll.text = string.Format("Yaw: {0:F3}, Pitch: {1:F3}, Roll: {2:F3}", packet.yaw, packet.pitch, packet.roll);
+                switch (packet.header.MagicNumber)
+                {
 
-                
-                
+                    case 20230903: //moai imu packet
+                        // Debug.Log("Magic Number is 20230903");
+
+                        textNetWorkFps.text = (1.0f / deltaTime).ToString("F2");
+
+                        //소숫점 3자리까지만 표시
+                        textQuaternionValues.text = string.Format("qW: {0:F3}, qX: {1:F3}, qY: {2:F3}, qZ: {3:F3}", packet.qW, packet.qX, packet.qY, packet.qZ);
+                        textAccelValues.text = string.Format("AccelX: {0:F3}, AccelY: {1:F3}, AccelZ: {2:F3}", packet.aX, packet.aY, packet.aZ);
+                        textGyroValues.text = string.Format("GyroX: {0:F3}, GyroY: {1:F3}, GyroZ: {2:F3}", packet.gX, packet.gY, packet.gZ);
+                        textMagValues.text = string.Format("MagX: {0:F3}, MagY: {1:F3}, MagZ: {2:F3}", packet.mX, packet.mY, packet.mZ);
+                        textBattery.text = string.Format("{0:F3}", packet.battery);
+                        textFireCount.text = string.Format("{0}", packet.fire_count);
+                        textDevId.text = string.Format("{0}", packet.dev_id);
+                        textYawPitchRoll.text = string.Format("Yaw: {0:F3}, Pitch: {1:F3}, Roll: {2:F3}", packet.yaw, packet.pitch, packet.roll);
+
+                        break;
+                    default:
+                        // Debug.Log("Magic Number is not 20230903");
+                        Debug.Log("unknown Magic Number :" + packet.header.MagicNumber);
+                        break;
+
+
+
+                }
+
+
+
+
+
             }
             catch (OperationCanceledException)
             {
@@ -80,7 +100,7 @@ public class packetMonitor : MonoBehaviour
                 Debug.LogError(ex);
             }
         }
-        
+
     }
 
     // Update is called once per frame
